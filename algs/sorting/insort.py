187fba9195e74078a82c48273f246859ea605bcb
Antash <4nt45h@gmail.com>
2016-01-16 01:03:10 +0100

    added documenting comment for insort

--- algs/sorting/insort.py
+++ algs/sorting/insort.py
@@ -1,3 +1,9 @@
+# Simple implementation of a very simple sorting algorithm
+# on avarage O(n^2)
+# good when an array is almost sorted, yet remember that it's always better
+#   to retain an sorted array by inserting in the proper position using bisect.insort(*args)
+
+
 def insertion_sort(l):
     for i in range(1, len(l)):
         j = i - 1
