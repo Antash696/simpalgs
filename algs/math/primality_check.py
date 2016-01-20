2510c4aa74ab966fe359937c85e8ab88420f6d5a
Antash <4nt45h@gmail.com>
2016-01-20 21:53:29 +0100

    adds primality check

---
+++ algs/math/primality_check.py
@@ -0,0 +1,8 @@
+def is_prime(p):
+    if p % 2 == 0 and p != 2:
+        return False
+    for i in range(3, round(p ** 0.5) + 1, 2):
+        if p % i == 0:
+            print (p, i)
+            return False
+    return True
