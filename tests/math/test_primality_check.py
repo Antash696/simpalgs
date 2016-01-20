9a8c2bf82467036ac40fc08cfece5c9567dbaf38
Antash <4nt45h@gmail.com>
2016-01-20 21:52:46 +0100

    adds tests for primality check

---
+++ tests/math/test_primality_check.py
@@ -0,0 +1,17 @@
+import unittest
+from algs.math.primality_check import is_prime
+
+
+class TestIsPrime(unittest.TestCase):
+
+    def test_is_13_prime(self):
+        self.assertTrue(is_prime(13))
+
+    def test_is_2_prime(self):
+        self.assertTrue(is_prime(13))
+
+    def test_is_4_prime(self):
+        self.assertFalse(is_prime(4))
+
+    def test_is_22_prime(self):
+        self.assertFalse(is_prime(22))
