1aa385503ca4e154ea7ac96a827c97b778330b25
Antash <4nt45h@gmail.com>
2016-01-20 17:34:21 +0100

    adds second LCM algo

--- algs/math/LCM.py
+++ algs/math/LCM.py
@@ -3,3 +3,10 @@ from GCD import GCD
 
 def LCM(x, y):  # for multiple x, y use recursion.
     return x * y // GCD(x, y)
+
+
+def LCM2(x, y):
+    lcm = x
+    while lcm % y != 0:
+        lcm += x
+    return lcm
